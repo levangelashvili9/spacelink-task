37b3c945709cde23865c0cedf4681db0abd4c0c5
Levan Gelashvili <levan.gelashvili9@gmail.com>
2024-03-28 16:23:20 +0400

    first commit

---
+++ app/ui/layout/navbar.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <Image
+        src="/images/identity-hub-logo.png"
+        alt="itentity hub logo"
+        width={163}
+        height={32}
+      />
+    </div>
+  );
+};

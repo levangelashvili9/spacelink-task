73522e7545fbb20eb219a194b808bc8d31b52b3d
Levan Gelashvili <levan.gelashvili9@gmail.com>
2024-03-29 20:15:01 +0400

    chore: import icons

---
+++ app/ui/icons/IconInactiveTick.tsx
@@ -0,0 +1,33 @@
+export const IconInactiveTick = () => {
+  return (
+    <svg
+      width="20.000000"
+      height="20.000000"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <desc>Created with Pixso.</desc>
+      <defs />
+      <g opacity="0.600000">
+        <path
+          id="Vector"
+          d="M9.99 1.66C5.39 1.66 1.66 5.39 1.66 10C1.66 14.6 5.39 18.33 9.99 18.33C14.59 18.33 18.33 14.6 18.33 10C18.33 5.39 14.59 1.66 9.99 1.66Z"
+          stroke="#CAD1E9"
+          strokeOpacity="1.000000"
+          strokeWidth="1.500000"
+        />
+        <path
+          id="Vector"
+          d="M6.66 10.41L8.74 12.5L13.33 7.91"
+          stroke="#CAD1E9"
+          strokeOpacity="1.000000"
+          strokeWidth="1.500000"
+          strokeLinejoin="round"
+          strokeLinecap="round"
+        />
+      </g>
+    </svg>
+  );
+};
--- app/ui/icons/index.ts
+++ app/ui/icons/index.ts
@@ -5,3 +5,5 @@ export { IconPhone } from "./IconPhone";
 export { IconCard } from "./IconCard";
 export { IconPassword } from "./IconPassword";
 export { IconLock } from "./IconLock";
+export { IconActiveTick } from "./IconActiveTick";
+export { IconInactiveTick } from "./IconInactiveTick";
